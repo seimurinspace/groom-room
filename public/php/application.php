b3021a7b5c6c192f3281e54daf218fd8c5b03a48
Макс <seimurinspace@mail.ru>
2022-01-19 20:40:55 +0300

    fixed some problems

--- public/php/application.php
+++ public/php/application.php
@@ -4,22 +4,22 @@ require_once ('connect.php');
 
 echo '<br>';
 //add application to table
+
 function addApp($connect, $name, $category) {
-  $date = date('Y-m-d');
-  $sql = "INSERT INTO `applications` (`id`, `name`, `img-before`, `img-after`, `timestamp`, `category`, `status`) VALUES (NULL, '$name', '1', '1', '$date', '$category', 'new')";
-  if ($conn->query($sql) === TRUE) {
+  $sql = "INSERT INTO `applications` (`id`, `name`, `img-before`, `img-after`, `timestamp`, `category`, `status`) VALUES (NULL, '$name', '1', '1', '$date', '$category', 'new')"; // добавялет наши данные
+  if ($connect->query($sql) === TRUE) { //еслиз запрос прошёл успешно
     echo "New application created successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . $connect->error;
+    echo "Error: " . $sql . "<br>" . $connect->error; //пошёл в жопу
   }
 }
 
 //output all app
 function getAllApps($connect) {
-  $applications =  mysqli_query($connect, "SELECT * FROM `applications`");
-  $vars = [];
+  $applications =  mysqli_query($connect, "SELECT * FROM `applications`"); //выбирает всё, что можно нах*й
+  $vars = [];//нужен, чтобы потом всё сюда засунуть с arraypush
   while ($var = mysqli_fetch_assoc($applications)) {
-    array_push($vars, [$var['id'], $var['name'], $var['img-before'], $var['img-after'], $var['timestamp'], $var['category'], $var['status']]);
+    array_push($vars, [$var['id'], $var['name'], $var['img-before'], $var['img-after'], $var['timestamp'], $var['category'], $var['status']]);//крутая штука, советую
   }
   for ($i=0; $i < count($vars); $i++) {
     for ($j=0; $j < count($vars[$i]); $j++) { 
@@ -32,12 +32,12 @@ function getAllApps($connect) {
 //output app by id
 function getAppsById($connect, $id) {
   for ($idx=0; $idx < count($id); $idx++) { 
-    $applications =  mysqli_query($connect, "SELECT * FROM `applications` Where `id` = '$id[$idx]'");
-    $vars = [];
+    $applications =  mysqli_query($connect, "SELECT * FROM `applications` Where `id` = '$id[$idx]'"); //выбирает чисто по ади
+    $vars = []; //нужен, чтобы потом всё сюда засунуть с arraypush
     while ($var = mysqli_fetch_assoc($applications)) {
-      array_push($vars, [$var['id'], $var['name'], $var['img-before'], $var['img-after'], $var['timestamp'], $var['category'], $var['status']]);
+      array_push($vars, [$var['id'], $var['name'], $var['img-before'], $var['img-after'], $var['timestamp'], $var['category'], $var['status']]); //крутая штука, советую
     }
-    for ($i=0; $i < count($vars); $i++) {
+    for ($i=0; $i < count($vars); $i++) { 
       for ($j=0; $j < count($vars[$i]); $j++) { 
         echo $vars[$i][$j]. " ";
       }
@@ -47,18 +47,18 @@ function getAppsById($connect, $id) {
 }
 
 //change status for app by name
-function changeStatusApplicationByName($connect, $name, $status) {
-  $sql = "UPDATE `applications` SET `status` = '$status' WHERE `name` = '$name'";
-  if ($connect->query($sql) === TRUE) {
+function changeStatusAppsByName($connect, $name, $status) {
+  $sql = "UPDATE `applications` SET `status` = '$status' WHERE `name` = '$name'"; //запрос на замену, если имя в таблице равно нашему имени, которое выбрали
+  if ($connect->query($sql) === TRUE) { //если запрос крут и он выдал нам тру
     echo "Status was changed successfull";
   } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error; //пошёл в жопу
   }
 }
 
 //remove app by name
-function removeApplicationByName($connect, $name) {
-  $sql = "DELETE FROM `applications` WHERE `applications`.`name` = '$name'";
+function removeAppsByName($connect, $name) {
+  $sql = "DELETE FROM `applications` WHERE `applications`.`name` = '$name'"; 
   if ($connect->query($sql) === TRUE) {
     echo "Application was remove successfull";
   } else {
--- public/php/user.php
+++ public/php/user.php
@@ -5,24 +5,24 @@ require_once ('connect.php');
 echo '<br>';
 //registration
 function reg($connect, $login, $password) {
-  $sql = "INSERT INTO `users` (`login`, `password`, `role`) VALUES ('$login', '$password', 'guest')";
-  if ($connect->query($sql) === TRUE) {
+  $sql = "INSERT INTO `users` (`login`, `password`, `role`) VALUES ('$login', '$password', 'guest')"; //сразу засовываем роль гостя
+  if ($connect->query($sql) === TRUE) { //если запрос выдал тру
     echo "New user created successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . $connect->error;
+    echo "Error: " . $sql . "<br>" . $connect->error; //пошёл в жопу и коннект рвётся
   }
 }
 //authorization
 function auth($connect, $login, $password) {
   $users =  mysqli_query($conn, "SELECT * FROM `users`");
   $vars = [];
-  while ($var = mysqli_fetch_assoc($users)) {
-    array_push($vars, [$var['login'], $var['password']]);
+  while ($var = mysqli_fetch_assoc($users)) { //рулит этой игрой, позволяя высунуть всё в виде некого массива и приравнять к массиву переменную 
+    array_push($vars, [$var['login'], $var['password']]); // просто выплёвываем логины и пароли в vars
   }
   $result = '';
-  for ($i=0; $i < count($vars); $i++) {
-    if ($login === $vars[$i][0] && $password === $vars[$i][1]) {
-      $result = 'win';
+  for ($i=0; $i < count($vars); $i++) { //просто делаем цЫкл, чтобы проверять всё и вся внизу
+    if ($login === $vars[$i][0] && $password === $vars[$i][1]) { // проверяем совпадения между введёнными логином и паролем и всеми данными (логин и пароль) 
+      $result = 'win'; //выплёвываем результат на победу
       break;
     }
   }
